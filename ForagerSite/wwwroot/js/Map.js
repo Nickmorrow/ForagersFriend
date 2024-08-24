6b5b744f2e321dcd7d819e9da3e16dcd4668f300
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-08-24 19:41:55 -0400

    On creating a pin, if you click another spot on the map without saving, the pin will be deleted and a new one will appear where you clicked. if you close the pop up before saving the pin will also be deleted.

--- ForagerSite/wwwroot/js/Map.js
+++ ForagerSite/wwwroot/js/Map.js
@@ -1,97 +1,9 @@
 ﻿
 
 window.map = null;
-
 var markers = {};
-//window.initializeMap = function (userFindsViewModels) {
+var tempMarker = null; 
 
-//    if (window.map) {
-//        console.log('Map already initialized, updating markers.');
-//        updateMarkers(userFindsViewModels);
-//        return;
-//    }
-
-//    window.map = L.map('map').setView([51.505, -0.09], 13);
-
-//    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//    }).addTo(window.map);
-
-//    updateMarkers(userFindsViewModels);
-//    window.map.on('click', function (e) {
-//        var tempId = 'temp-' + Date.now();
-//        var newMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.map);
-
-//        markers[tempId] = {
-//            marker: newMarker,
-//            lat: e.latlng.lat,
-//            lng: e.latlng.lng
-//        };
-
-//        var popupContent = `<form id="UserFindForm_${tempId}">
-//                                <label for="findName">Name:</label>
-//                                <input type="text" id="UsfName" name="findName"><br>
-
-//                                <label for="speciesName">Species Name:</label>
-//                                <input type="text" id="UsfSpeciesName" name="speciesName"><br>
-
-//                                <label for="speciesType">Species Type:</label>
-//                                <input type="text" id="UsfSpeciesType" name="speciesType"><br>
-
-//                                <label for="useCategory">Use Category:</label>
-//                                <input type="text" id="UsfUseCategory" name="useCategory"><br>
-
-//                                <label for="features">Distinguishing Features:</label>
-//                                <input type="text" id="UsfFeatures" name="features"><br>
-
-//                                <label for="lookalikes">Dangerous Lookalikes:</label>
-//                                <input type="text" id="UsfLookAlikes" name="lookalikes"><br>
-
-//                                <label for="harvestMethod">Harvest Method:</label>
-//                                <input type="text" id="UsfHarvestMethod" name="harvestMethod"><br>
-
-//                                <label for="tastesLike">Tastes Like:</label>
-//                                <input type="text" id="UsfTastesLike" name="tastesLike"><br>
-
-//                                <label for="description">Notes:</label>
-//                                <input type="text" id="UsfDescription" name="description"><br>
-
-//                                <button type="button" onclick="createFind('${tempId}', ${e.latlng.lat}, ${e.latlng.lng})">Save</button>
-//                            </form>`;
-
-//        newMarker.bindPopup(popupContent).openPopup();
-//    });
-//}
-
-//function updateMarkers(userFindLocations) {
-//    // Clear existing markers
-//    for (var key in markers) {
-//        if (markers.hasOwnProperty(key)) {
-//            markers[key].marker.remove();
-//        }
-//    }
-//    markers = {}; // Reset markers object
-
-//    // Add new markers
-//    userFindLocations.forEach(location => {
-//        var marker = L.marker([location.uslLatitude, location.uslLongitude]).addTo(window.map);
-//        var findId = location.userFind.usfId;
-
-//        var popupContent = `<p><strong>Name:</strong> ${location.userFind.usfName}</p>                            
-//                            <p><strong>Species Name:</strong> ${location.userFind.usfSpeciesName}</p>
-//                            <p><strong>Species Type:</strong> ${location.userFind.usfSpeciesType}</p>
-//                            <p><strong>Use Category:</strong> ${location.userFind.usfUseCategory}</p>
-//                            <p><strong>Distinguishing Features:</strong> ${location.userFind.usfFeatures}</p>
-//                            <p><strong>Dangerous Lookalikes:</strong> ${location.userFind.usfLookAlikes}</p>
-//                            <p><strong>Harvest Method:</strong> ${location.userFind.usfHarvestMethod}</p>
-//                            <p><strong>Tastes Like:</strong> ${location.userFind.usfTastesLike}</p>
-//                            <p><strong>Notes:</strong> ${location.userFind.usfDescription}</p>
-//                            <button type="button" onclick="updateFind('${findId}')">Edit</button>
-//                            <button type="button" onclick="deleteFind('${findId}')">Delete</button>`;                                                                        
-//        marker.bindPopup(popupContent);
-//        markers[findId] = { marker: marker, lat: location.uslLatitude, lng: location.uslLongitude };
-//    });
-//}
 window.initializeMap = function (json, currentUserId) {
 
     let userFindsViewModels = JSON.parse(json);
@@ -110,14 +22,21 @@ window.initializeMap = function (json, currentUserId) {
 
     updateMarkers(userFindsViewModels, currentUserId);
     window.map.on('click', function (e) {
-        var tempId = 'temp-' + Date.now();
-        var newMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.map);
+        //var tempId = 'temp-' + Date.now();
+        //var newMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.map);
+        if (tempMarker) {
+            window.map.removeLayer(tempMarker); // Remove the existing temporary marker
+            tempMarker = null;
+        }
 
-        markers[tempId] = {
-            marker: newMarker,
-            lat: e.latlng.lat,
-            lng: e.latlng.lng
-        };
+        var tempId = 'temp-' + Date.now();
+        tempMarker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(window.map);
+
+        //markers[tempId] = {
+        //    marker: newMarker,
+        //    lat: e.latlng.lat,
+        //    lng: e.latlng.lng
+        //};
 
         var popupContent = `<form id="UserFindForm_${tempId}">
                                 <label for="findName">Name:</label>
@@ -150,7 +69,14 @@ window.initializeMap = function (json, currentUserId) {
                                 <button type="button" onclick="createFind('${tempId}', ${e.latlng.lat}, ${e.latlng.lng})">Save</button>
                             </form>`;
 
-        newMarker.bindPopup(popupContent).openPopup();
+        //newMarker.bindPopup(popupContent).openPopup();
+        tempMarker.bindPopup(popupContent);
+        tempMarker.openPopup(); 
+
+        tempMarker.on('popupclose', function () {
+            map.removeLayer(tempMarker);
+            tempMarker = null;
+        });
     });
 }
 
@@ -161,6 +87,7 @@ function updateMarkers(userFindsViewModels, currentUserId) {
     markers = {}; 
 
     userFindsViewModels.forEach(viewModel => {
+
         viewModel.userFindLocations.forEach(location => {           
 
             if (location.UslLatitude !== undefined && location.UslLongitude !== undefined) {
@@ -189,6 +116,7 @@ function updateMarkers(userFindsViewModels, currentUserId) {
                             popupContent += `<button type="button" onclick="updateFind('${findId}')">Edit</button>
                                              <button type="button" onclick="deleteFind('${findId}')">Delete</button>`;
                         }
+                        console.log("viewmodel id:", viewModel.user.UsrId, "currentUserId :", currentUserId)
 
                         marker.bindPopup(popupContent);
                         markers[findId] = { marker: marker, lat: lat, lng: lng };
@@ -287,6 +215,12 @@ window.submitUpdatedFind = function (findId, lat, lng) {
 
 
 window.createFind = function (tempId, lat, lng) {
+
+    if (tempMarker) {
+        tempMarker.closePopup();  
+        tempMarker = null;        
+    }
+
     if (!tempId) {
         console.error('Invalid tempId:', tempId);
         return;
