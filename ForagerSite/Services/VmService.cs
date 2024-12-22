84705e102ae781c0214e4a607cca9f7d100124f0
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-12-22 01:12:50 -0500

    Fixed issue where finds could not be updated when map filter is ster to my finds, when set to all finds filter the edit field still shows empty

--- ForagerSite/Services/VmService.cs
+++ ForagerSite/Services/VmService.cs
@@ -13,7 +13,7 @@ namespace ForagerSite.Services
         public List<UserFindsViewModel> MyViewModels { get; set; } = new();
         public List<UserFindsViewModel> AllViewModels { get; set; } = new();
         public List<UserFindsViewModel> CurrentViewModels { get; set; } = new();
-        public void AddOrUpdateViewModel(Guid userId, UserFindsViewModel viewModel)
+        public void AddViewModel(Guid userId, UserFindsViewModel viewModel)
         {
             var existingViewModel = CurrentViewModels.FirstOrDefault(vm => vm.user.UsrId == userId);
 
@@ -34,10 +34,50 @@ namespace ForagerSite.Services
                 backupVmlAll.userFinds.Add(viewModel.userFinds[0]);
                 backupVmlAll.userFindLocations.Add(viewModel.userFindLocations[0]);
                 backupVmlAll.userImages.AddRange(viewModel.userImages);
-            }            
+            }          
+        }
+        public void UpdateViewModel(Guid userId, UserFindsViewModel viewModel)
+        {
+            // Update Vm
+            var existingViewModel = CurrentViewModels.FirstOrDefault(vm => vm.user.UsrId == userId);
+
+            existingViewModel.userFinds.Remove(existingViewModel.userFinds.Where(f => f.UsFId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+            existingViewModel.userFinds.Add(viewModel.userFinds[0]);
+
+            existingViewModel.userFindLocations.Remove(existingViewModel.userFindLocations.Where(fl => fl.UslUsfId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+            existingViewModel.userFindLocations.Add(viewModel.userFindLocations[0]);
+
+            existingViewModel.userImages.RemoveAll(fi => fi.UsiUsfId == viewModel.userFinds[0].UsFId);
+            existingViewModel.userImages.AddRange(viewModel.userImages);
+
+            // Backup Vm My filter
+            var backupVmlMy = MyViewModels.FirstOrDefault(vm => vm.user.UsrId == userId); 
+
+            backupVmlMy.userFinds.Remove(backupVmlMy.userFinds.Where(f => f.UsFId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+            backupVmlMy.userFinds.Add(viewModel.userFinds[0]);
+
+            backupVmlMy.userFindLocations.Remove(backupVmlMy.userFindLocations.Where(fl => fl.UslUsfId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+            backupVmlMy.userFindLocations.Add(viewModel.userFindLocations[0]);
+
+            backupVmlMy.userImages.RemoveAll(fi => fi.UsiUsfId == viewModel.userFinds[0].UsFId);
+            backupVmlMy.userImages.AddRange(viewModel.userImages);
+
+            // Backup Vm All filter
+            if (AllViewModels.Count > 0) 
+            {
+                var backupVmlAll = AllViewModels.FirstOrDefault(vm => vm.user.UsrId == userId);
+
+                backupVmlAll.userFinds.Remove(backupVmlAll.userFinds.Where(f => f.UsFId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+                backupVmlAll.userFinds.Add(viewModel.userFinds[0]);
+
+                backupVmlAll.userFindLocations.Remove(backupVmlAll.userFindLocations.Where(fl => fl.UslUsfId == viewModel.userFinds[0].UsFId).FirstOrDefault());
+                backupVmlAll.userFindLocations.Add(viewModel.userFindLocations[0]);
+
+                backupVmlAll.userImages.RemoveAll(fi => fi.UsiUsfId == viewModel.userFinds[0].UsFId);
+                backupVmlAll.userImages.AddRange(viewModel.userImages);
+            }
         }
 
-        // Get a user's view model
         public UserFindsViewModel GetViewModel(Guid userId)
         {
             return CurrentViewModels.FirstOrDefault(vm => vm.user.UsrId == userId);
--- ForagerSite/Utilities/VmUtilities.cs
+++ ForagerSite/Utilities/VmUtilities.cs
@@ -5,7 +5,7 @@ namespace ForagerSite.Utilities
     public static class VmUtilities
     {
 
-        public static T DeepCopy<T>(T obj)
+        public static T Copy<T>(T obj)
         {
             var settings = new JsonSerializerSettings
             {
