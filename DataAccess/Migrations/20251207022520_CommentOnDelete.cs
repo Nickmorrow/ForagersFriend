db9a08bb0d062f45a2388fbe88f598a29440a845
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-06 21:33:03 -0500

    Migration to fix usercomment parent child behavior

---
+++ DataAccess/Migrations/20251207022520_CommentOnDelete.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentOnDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFindsComments_UserFindsComments_UscParentCommentId",
+                table: "UserFindsComments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFindsComments_UserFindsComments_UscParentCommentId",
+                table: "UserFindsComments",
+                column: "UscParentCommentId",
+                principalTable: "UserFindsComments",
+                principalColumn: "UscId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserFindsComments_UserFindsComments_UscParentCommentId",
+                table: "UserFindsComments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserFindsComments_UserFindsComments_UscParentCommentId",
+                table: "UserFindsComments",
+                column: "UscParentCommentId",
+                principalTable: "UserFindsComments",
+                principalColumn: "UscId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
