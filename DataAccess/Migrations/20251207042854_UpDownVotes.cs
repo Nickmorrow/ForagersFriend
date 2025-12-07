5e52208ae0325858110cdfe7097ff5b47dc5d90a
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-06 23:44:46 -0500

    Added thumbs up and down icons to user find

---
+++ DataAccess/Migrations/20251207042854_UpDownVotes.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpDownVotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UscDownVotes",
+                table: "UserFindsComments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UscUpVotes",
+                table: "UserFindsComments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsfDownVotes",
+                table: "UserFinds",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsfUpVotes",
+                table: "UserFinds",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UscDownVotes",
+                table: "UserFindsComments");
+
+            migrationBuilder.DropColumn(
+                name: "UscUpVotes",
+                table: "UserFindsComments");
+
+            migrationBuilder.DropColumn(
+                name: "UsfDownVotes",
+                table: "UserFinds");
+
+            migrationBuilder.DropColumn(
+                name: "UsfUpVotes",
+                table: "UserFinds");
+        }
+    }
+}
