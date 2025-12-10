95f63d1b402c4bb194ee64a54a69a8d9d9bbb552
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-09 21:23:06 -0500

    Implemented Log out

--- ForagerSite/Services/UserStateService.cs
+++ ForagerSite/Services/UserStateService.cs
@@ -7,11 +7,19 @@ namespace ForagerSite.Services
         public bool IsAuthenticated { get; set; }
         public UserViewModel CurrentUser { get; set; }
 
+        public event Action? OnChange;
+
         public void SetUserState(bool isAuthenticated, UserViewModel currentUser)
         {
             IsAuthenticated = isAuthenticated;
             CurrentUser = currentUser;
             Console.WriteLine($"User authenticated: {IsAuthenticated}, User: {CurrentUser?.user?.UsrName}");
+            OnChange?.Invoke();
+        }
+
+        public void Logout()
+        {
+            SetUserState(false, null);
         }
     }
 }
