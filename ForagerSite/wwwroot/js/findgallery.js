83f3373ed38c38cd852d329c082cfa3bdd99a6fc
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-16 16:53:34 -0500

    Added a popup image gallery

---
+++ ForagerSite/wwwroot/js/findgallery.js
@@ -0,0 +1,8 @@
+﻿window.findGallery = {
+    scrollToChild: function (container, index) {
+        if (!container) return;
+        const child = container.children[index];
+        if (!child) return;
+        child.scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    }
+};
