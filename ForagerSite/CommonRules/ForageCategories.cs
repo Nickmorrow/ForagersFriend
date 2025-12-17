ce350206d6f110f4d0d072f4caa4e577ab01853c
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-17 14:05:03 -0500

    Added new Categories options to create and edit forms

--- ForagerSite/CommonRules/ForageCategories.cs
+++ ForagerSite/CommonRules/ForageCategories.cs
@@ -1,84 +1,88 @@
 ﻿namespace ForagerSite.CommonRules
 {
-    public static class ForageCategories
+    namespace ForagerSite.CommonRules
     {
-        public enum ForageCategory
+        public static class ForageCategories
         {
-            Plants,
-            Fungi,
-            Fruits,
-            NutsAndSeeds,
-            HerbsAndMedicinals,
-            Aquatic,
-            AnimalDerived,
-            Trees,
-            UtilityAndCraft
-        }
-
-        public static readonly IReadOnlyDictionary<ForageCategory, IReadOnlyList<string>> Subcategories =
-            new Dictionary<ForageCategory, IReadOnlyList<string>>
+            public static readonly List<string> ForageCategory = new()
             {
-                [ForageCategory.Plants] = new List<string>
-                {
-                "Leaves & Greens",
-                "Flowers",
-                "Shoots & Stems",
-                "Roots, Tubers & Rhizomes",
-                "Dye Plants"
-                },
-
-                [ForageCategory.Fungi] = new List<string>
-                {
-                "Edible Mushrooms",
-                "Medicinal / Functional"
-                },
-
-                [ForageCategory.Fruits] = new List<string>
-                {
-                "Tree Fruits",
-                "Berries",
-                "Vines & Ground Fruits"
-                },
-
-                [ForageCategory.NutsAndSeeds] = new List<string>
-                {
-                "Tree Nuts",
-                "Seeds & Grains"
-                },
-
-                [ForageCategory.HerbsAndMedicinals] = new List<string>
-                {
-                "Culinary Herbs",
-                "Medicinal Plants"
-                },
-
-                [ForageCategory.Aquatic] = new List<string>
-                {
-                "Aquatic Plants",
-                "Shellfish & Crustaceans"
-                },
-
-                [ForageCategory.AnimalDerived] = new List<string>
-                {
-                "Honey & Resins",
-                "Insects"
-                },
-
-                [ForageCategory.Trees] = new List<string>
-                {
-                "Sap & Syrups",
-                "Needles, Tips & Bark",
-                "Inner Bark & Cambium"
-                },
-
-                [ForageCategory.UtilityAndCraft] = new List<string>
-                {
-                "Utility & Craft Materials"
-                }
-
+                "Plants",
+                "Fungi",
+                "Fruits",
+                "NutsAndSeeds",
+                "HerbsAndMedicinals",
+                "Aquatic",
+                "AnimalDerived",
+                "Trees",
+                "UtilityAndCraft"
             };
 
-        public static IReadOnlyList<string> GetSubcategories(ForageCategory category)
-            => Subcategories.TryGetValue(category, out var subs) ? subs : Array.Empty<string>();
+
+            public static readonly IReadOnlyDictionary<string, IReadOnlyList<string>> Subcategories =
+                new Dictionary<string, IReadOnlyList<string>>
+                {
+                    ["Plants"] = new[]
+                    {
+                        "Leaves & Greens",
+                        "Flowers",
+                        "Shoots & Stems",
+                        "Roots, Tubers & Rhizomes",
+                        "Dye Plants"
+                    },
+
+                    ["Fungi"] = new[]
+                    {
+                        "Edible Mushrooms",
+                        "Medicinal / Functional"
+                    },
+
+                    ["Fruits"] = new[]
+                    {
+                        "Tree Fruits",
+                        "Berries",
+                        "Vines & Ground Fruits"
+                    },
+
+                    ["NutsAndSeeds"] = new[]
+                    {
+                        "Tree Nuts",
+                        "Seeds & Grains"
+                    },
+
+                    ["HerbsAndMedicinals"] = new[]
+                    {
+                        "Culinary Herbs",
+                        "Medicinal Plants"
+                    },
+
+                    ["Aquatic"] = new[]
+                    {
+                        "Aquatic Plants",
+                        "Shellfish & Crustaceans"
+                    },
+
+                    ["AnimalDerived"] = new[]
+                    {
+                        "Honey & Resins",
+                        "Insects"
+                    },
+
+                    ["Trees"] = new[]
+                    {
+                        "Sap & Syrups",
+                        "Needles, Tips & Bark",
+                        "Inner Bark & Cambium"
+                    },
+
+                    ["UtilityAndCraft"] = new[]
+                    {
+                        "Utility & Craft Materials"
+                    }
+                };
+
+            //public static IReadOnlyList<string> GetSubcategories(List<string> category)
+            //    => Subcategories.TryGetValue(category, out var subs) ? subs : Array.Empty<string>();
+        }
     }
+
 }
