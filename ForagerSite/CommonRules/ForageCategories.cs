24b087fb24c2a324671a83a042d1ebcc66d5abf8
Nick Morrow <nicholasjohnmorrow@gmail.com>
2025-12-17 13:03:27 -0500

    Added find category and search rules classes, commented out some find fiels that i no longer want

---
+++ ForagerSite/CommonRules/ForageCategories.cs
@@ -0,0 +1,84 @@
+﻿namespace ForagerSite.CommonRules
+{
+    public static class ForageCategories
+    {
+        public enum ForageCategory
+        {
+            Plants,
+            Fungi,
+            Fruits,
+            NutsAndSeeds,
+            HerbsAndMedicinals,
+            Aquatic,
+            AnimalDerived,
+            Trees,
+            UtilityAndCraft
+        }
+
+        public static readonly IReadOnlyDictionary<ForageCategory, IReadOnlyList<string>> Subcategories =
+            new Dictionary<ForageCategory, IReadOnlyList<string>>
+            {
+                [ForageCategory.Plants] = new List<string>
+                {
+                "Leaves & Greens",
+                "Flowers",
+                "Shoots & Stems",
+                "Roots, Tubers & Rhizomes",
+                "Dye Plants"
+                },
+
+                [ForageCategory.Fungi] = new List<string>
+                {
+                "Edible Mushrooms",
+                "Medicinal / Functional"
+                },
+
+                [ForageCategory.Fruits] = new List<string>
+                {
+                "Tree Fruits",
+                "Berries",
+                "Vines & Ground Fruits"
+                },
+
+                [ForageCategory.NutsAndSeeds] = new List<string>
+                {
+                "Tree Nuts",
+                "Seeds & Grains"
+                },
+
+                [ForageCategory.HerbsAndMedicinals] = new List<string>
+                {
+                "Culinary Herbs",
+                "Medicinal Plants"
+                },
+
+                [ForageCategory.Aquatic] = new List<string>
+                {
+                "Aquatic Plants",
+                "Shellfish & Crustaceans"
+                },
+
+                [ForageCategory.AnimalDerived] = new List<string>
+                {
+                "Honey & Resins",
+                "Insects"
+                },
+
+                [ForageCategory.Trees] = new List<string>
+                {
+                "Sap & Syrups",
+                "Needles, Tips & Bark",
+                "Inner Bark & Cambium"
+                },
+
+                [ForageCategory.UtilityAndCraft] = new List<string>
+                {
+                "Utility & Craft Materials"
+                }
+
+            };
+
+        public static IReadOnlyList<string> GetSubcategories(ForageCategory category)
+            => Subcategories.TryGetValue(category, out var subs) ? subs : Array.Empty<string>();
+    }
+}
---
+++ ForagerSite/CommonRules/SearchRules.cs
@@ -0,0 +1,30 @@
+﻿using ForagerSite.DataContainer;
+using static ForagerSite.CommonRules.SearchRules;
+
+namespace ForagerSite.CommonRules
+{
+
+    public static class SearchRules
+    {
+        public enum MapFilters
+        {
+            UserOnly,
+            AllUsers,
+            FriendUsers
+        }
+
+        public enum VisibilityFilters
+        {
+            Public,
+            Private,
+            Friends
+        }
+        public enum SortMode
+        {
+            Distance,
+            DateDiscovered,
+            Popularity
+        }
+    }
+    
+}
